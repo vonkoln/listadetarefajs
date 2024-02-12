0bd36d924db6195b80f4c06842c49fe98a8a062b
Bruno Stefano de Souza <40743118+vonkoln@users.noreply.github.com>
2024-02-12 21:51:37 +0000

    ..

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const novoItemInput = document.getElementById("input-novo-item")
+
+    const botaoAdicionarItem = document.getElementsByClassName("botao-adicionar")[0]
+    
+    
+})
+
+
+
